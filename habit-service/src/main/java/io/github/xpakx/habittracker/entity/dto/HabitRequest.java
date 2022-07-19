ee2be57a5ec5fdb4459cf130ab499de8bba4a986
xpakx <55586470+xpakx@users.noreply.github.com>
2022-07-19 13:15:23 +0200

    feat(backend): add service method to add new habit

---
+++ habit-service/src/main/java/io/github/xpakx/habittracker/entity/dto/HabitRequest.java
@@ -0,0 +1,22 @@
+package io.github.xpakx.habittracker.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HabitRequest {
+    private String name;
+    private String description;
+    private Integer interval;
+    private LocalDateTime start;
+
+    private Long contextId;
+    private String triggerName;
+}
---
+++ habit-service/src/main/java/io/github/xpakx/habittracker/service/HabitService.java
@@ -0,0 +1,33 @@
+package io.github.xpakx.habittracker.service;
+
+import io.github.xpakx.habittracker.entity.Habit;
+import io.github.xpakx.habittracker.entity.HabitTrigger;
+import io.github.xpakx.habittracker.entity.dto.HabitRequest;
+import io.github.xpakx.habittracker.repo.HabitContextRepository;
+import io.github.xpakx.habittracker.repo.HabitRepository;
+import io.github.xpakx.habittracker.repo.HabitTriggerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class HabitService {
+    private final HabitRepository habitRepository;
+    private final HabitContextRepository contextRepository;
+    private final HabitTriggerRepository triggerRepository;
+
+    @Transactional
+    public Habit addHabit(HabitRequest request) {
+        Habit habit = new Habit();
+        habit.setName(request.getName());
+        habit.setDescription(request.getDescription());
+        habit.setInterval(request.getInterval());
+        habit.setStart(request.getStart());
+        habit.setContext(contextRepository.getReferenceById(request.getContextId()));
+        HabitTrigger trigger = new HabitTrigger();
+        trigger.setName(request.getTriggerName());
+        habit.setTrigger(trigger);
+        return habitRepository.save(habit);
+    }
+}
