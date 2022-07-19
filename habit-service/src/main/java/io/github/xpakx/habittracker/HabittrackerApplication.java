44d86813e069abe3021a27752eae1eba0d91537c
xpakx <55586470+xpakx@users.noreply.github.com>
2022-07-19 12:53:18 +0200

    chore(backend): habit service

---
+++ habit-service/src/main/java/io/github/xpakx/habittracker/HabittrackerApplication.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.habittracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabittrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabittrackerApplication.class, args);
+	}
+
+}
---
+++ habit-service/src/test/java/io/github/xpakx/habittracker/HabittrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.habittracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabittrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
