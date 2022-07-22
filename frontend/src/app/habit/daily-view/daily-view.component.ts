b77e2a50a7ae5e8f4f9155d3070721ad3b91fb90
xpakx <55586470+xpakx@users.noreply.github.com>
2022-07-22 15:21:23 +0200

    feat(frontend): fetch daily habits from backend in daily view

--- frontend/src/app/habit/daily-view/daily-view.component.ts
+++ frontend/src/app/habit/daily-view/daily-view.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Habit } from '../dto/habit';
+import { HabitService } from '../habit.service';
 
 @Component({
   selector: 'app-daily-view',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./daily-view.component.css']
 })
 export class DailyViewComponent implements OnInit {
+  habits: Habit[] = [];
 
-  constructor() { }
+  constructor(private habitService: HabitService) { }
 
   ngOnInit(): void {
+    this.habitService.getHabitsForDate().subscribe({
+      next: (response: Habit[]) => this.updateHabitList(response),
+      error: (error: HttpErrorResponse) => this.showError(error)
+
+    });
+  }
+
+
+  updateHabitList(response: Habit[]): void {
+    this.habits = response;
+  }
+
+  showError(error: HttpErrorResponse): void {
+    //TODO
   }
 
 }
--- frontend/src/app/habit/habit.service.ts
+++ frontend/src/app/habit/habit.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Habit } from './dto/habit';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HabitService {
+  private apiServerUrl = environment.apiServerUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getHabitsForDate():  Observable<Habit[]> {
+    return this.http.get<Habit[]>(`${this.apiServerUrl}/habit/date`);
+  }
 }
