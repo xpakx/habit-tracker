683e17606424b4d0fd4188cc7579bfee942c53c3
xpakx <55586470+xpakx@users.noreply.github.com>
2022-07-24 15:21:33 +0200

    feat(frontent/ui): add side menu

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { DailyViewComponent } from './habit/daily-view/daily-view.component';
 import { HabitListComponent } from './habit/habit-list/habit-list.component';
 import { HabitComponent } from './habit/habit/habit.component';
+import { MenuComponent } from './navigation/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DailyViewComponent,
     HabitListComponent,
-    HabitComponent
+    HabitComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
