ada5630eac047abf84abee079b920dc5e62f32cc
xpakx <55586470+xpakx@users.noreply.github.com>
2022-08-05 12:15:29 +0200

    chore(gamification-service): init

---
+++ gamification-service/src/main/java/io/github/xpakx/habitgamification/HabitgamificationApplication.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.habitgamification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitgamificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitgamificationApplication.class, args);
+	}
+
+}
---
+++ gamification-service/src/test/java/io/github/xpakx/habitgamification/HabitgamificationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.habitgamification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitgamificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
