b9f9a0130433163f12a4f22a8752e1ed5b696af0
xpakx <55586470+xpakx@users.noreply.github.com>
2022-08-07 13:31:22 +0200

    feat(gamification-service): add basic achievement processor

---
+++ gamification-service/src/main/java/io/github/xpakx/habitgamification/gamification/BronzeAchievementProcessorImpl.java
@@ -0,0 +1,22 @@
+package io.github.xpakx.habitgamification.gamification;
+
+import io.github.xpakx.habitgamification.gamification.dto.HabitCompletion;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class BronzeAchievementProcessorImpl implements AchievementProcessor {
+    @Override
+    public Optional<Badge> process(HabitCompletion completion, int experience) {
+        if(experience >= 50) {
+            return Optional.of(type());
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Badge type() {
+        return Badge.BRONZE;
+    }
+}
