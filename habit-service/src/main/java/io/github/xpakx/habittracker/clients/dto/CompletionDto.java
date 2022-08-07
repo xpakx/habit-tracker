cf57a8cd27fbcf00463e34697544cd21ea646438
xpakx <55586470+xpakx@users.noreply.github.com>
2022-08-07 13:56:17 +0200

    feat(habit-service): add method to send info to gamification service

---
+++ habit-service/src/main/java/io/github/xpakx/habittracker/clients/dto/CompletionDto.java
@@ -0,0 +1,12 @@
+package io.github.xpakx.habittracker.clients.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CompletionDto {
+    Long completionId;
+    Long habitId;
+    Long userId;
+}
