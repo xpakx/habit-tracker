0dec56788f3a8cb2d14c6adb7a8be2b9c29f021d
xpakx <55586470+xpakx@users.noreply.github.com>
2022-08-10 13:38:21 +0200

    chore(habit-service): add amqp config

---
+++ habit-service/src/main/java/io/github/xpakx/habittracker/config/AMQPConfig.java
@@ -0,0 +1,24 @@
+package io.github.xpakx.habittracker.config;
+
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AMQPConfig {
+    private final String completionsTopic;
+
+    public AMQPConfig(@Value("${amqp.exchange.completions}") final String completionsTopic) {
+        this.completionsTopic = completionsTopic;
+    }
+
+    @Bean
+    public TopicExchange challengesTopicExchange() {
+        return ExchangeBuilder
+                .topicExchange(completionsTopic)
+                .durable(true)
+                .build();
+    }
+}
