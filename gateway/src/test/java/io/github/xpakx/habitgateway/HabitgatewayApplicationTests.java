39ca8c033f3b66e9e12327d9bb16207ac1c7aede
xpakx <55586470+xpakx@users.noreply.github.com>
2022-08-26 22:51:16 +0200

    chore(gateway): init

---
+++ gateway/src/test/java/io/github/xpakx/habitgateway/HabitgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.habitgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
