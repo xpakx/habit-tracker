8bbe74d68a475674f8c80cde80fef306e0ea587a
xpakx <55586470+xpakx@users.noreply.github.com>
2022-08-27 23:35:47 +0200

    fix(gamification): add jackson2message converter

--- gamification-service/src/main/java/io/github/xpakx/habitgamification/config/AMQPConfig.java
+++ gamification-service/src/main/java/io/github/xpakx/habitgamification/config/AMQPConfig.java
@@ -1,10 +1,14 @@
 package io.github.xpakx.habitgamification.config;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
 import org.springframework.messaging.handler.annotation.support.MessageHandlerMethodFactory;
 
 @Configuration
@@ -28,7 +32,18 @@ public class AMQPConfig {
     }
 
     @Bean
-    public RabbitListenerConfigurer rabbitListenerConfigurer(final MessageHandlerMethodFactory messageHandlerMethodFactory) {
+    public MessageHandlerMethodFactory messageHandlerMethodFactory() {
+        DefaultMessageHandlerMethodFactory factory = new DefaultMessageHandlerMethodFactory();
+        final MappingJackson2MessageConverter jsonConverter = new MappingJackson2MessageConverter();
+        jsonConverter.getObjectMapper().registerModule(
+                new ParameterNamesModule(JsonCreator.Mode.PROPERTIES));
+        factory.setMessageConverter(jsonConverter);
+        return factory;
+    }
+
+    @Bean
+    public RabbitListenerConfigurer rabbitListenerConfigurer(
+            final MessageHandlerMethodFactory messageHandlerMethodFactory) {
         return (c) -> c.setMessageHandlerMethodFactory(messageHandlerMethodFactory);
     }
 }
