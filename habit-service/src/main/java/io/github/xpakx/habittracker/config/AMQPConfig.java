8237138c0ff4220da19474a04890ae70f2ddfb6f
xpakx <55586470+xpakx@users.noreply.github.com>
2022-08-27 23:32:12 +0200

    fix(habit): add jackson2message converter

--- habit-service/src/main/java/io/github/xpakx/habittracker/config/AMQPConfig.java
+++ habit-service/src/main/java/io/github/xpakx/habittracker/config/AMQPConfig.java
@@ -2,6 +2,7 @@ package io.github.xpakx.habittracker.config;
 
 import org.springframework.amqp.core.ExchangeBuilder;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,5 +21,10 @@ public class AMQPConfig {
                 .topicExchange(completionsTopic)
                 .durable(true)
                 .build();
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }
