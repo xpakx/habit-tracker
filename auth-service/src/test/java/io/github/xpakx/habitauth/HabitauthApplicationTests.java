9adb4261842a83ccfecb80361e00d782f612e543
xpakx <55586470+xpakx@users.noreply.github.com>
2022-08-30 14:30:11 +0200

    chore(auth): init authentication microservice

---
+++ auth-service/src/test/java/io/github/xpakx/habitauth/HabitauthApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.habitauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
