135d8b330207283897b4cd66d220f2e29101fb17
xpakx <55586470+xpakx@users.noreply.github.com>
2022-08-30 14:09:07 +0200

    chore(city): init city management microservice

---
+++ city-service/src/main/java/io/github/xpakx/habitcity/HabitcityApplication.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.habitcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitcityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitcityApplication.class, args);
+	}
+
+}
---
+++ city-service/src/test/java/io/github/xpakx/habitcity/HabitcityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.habitcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitcityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
