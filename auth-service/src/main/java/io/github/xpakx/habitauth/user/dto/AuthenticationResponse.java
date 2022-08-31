5043df7fec42081071581cce64519391775eb280
xpakx <55586470+xpakx@users.noreply.github.com>
2022-08-31 13:49:41 +0200

    fix(auth): fix problem with sending response

--- auth-service/src/main/java/io/github/xpakx/habitauth/user/dto/AuthenticationResponse.java
+++ auth-service/src/main/java/io/github/xpakx/habitauth/user/dto/AuthenticationResponse.java
@@ -1,8 +1,10 @@
 package io.github.xpakx.habitauth.user.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class AuthenticationResponse {
     private String token;
     private String username;
