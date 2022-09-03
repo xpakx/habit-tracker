f68340b8bed64e9792a4c05aa4e852533a53342e
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-03 11:43:17 +0200

    feat(frontend): let user register new account

--- frontend/src/app/user/register/register.component.ts
+++ frontend/src/app/user/register/register.component.ts
@@ -1,4 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { AuthenticationResponse } from '../dto/authentication-response';
+import { RegisterForm } from '../form/register-form';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +11,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  form: FormGroup<RegisterForm>;
 
-  constructor() { }
+  constructor(private authService: AuthService, private fb: FormBuilder) { 
+    this.form = this.fb.nonNullable.group({
+      username: [new String(''), Validators.required],
+      password: [new String(''), Validators.required],
+      passwordRe: [new String(''), Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  saveToken(response: AuthenticationResponse): void {
+    localStorage.setItem("token", response.token);
+  }
+
+  showError(error: HttpErrorResponse): void {
+
+  }
+
+  register(): void {
+    if(this.form.valid) {
+      this.authService.register({ 
+        username: this.form.controls.username.value, 
+        password: this.form.controls.password.value,
+        passwordRe: this.form.controls.passwordRe.value
+      }).subscribe({
+        next: (response: AuthenticationResponse) => this.saveToken(response),
+        error: (error: HttpErrorResponse) => this.showError(error)
+      });
+    }
   }
 
 }
