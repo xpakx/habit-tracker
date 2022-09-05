dff98b37f644d562d187a350b37b5d0171221a86
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-05 13:00:05 +0200

    feat(city): add items to shop on refresh

--- city-service/src/main/java/io/github/xpakx/habitcity/resource/ResourceRepository.java
+++ city-service/src/main/java/io/github/xpakx/habitcity/resource/ResourceRepository.java
@@ -1,6 +1,11 @@
 package io.github.xpakx.habitcity.resource;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
+    @Query(nativeQuery = true, value = "select r from resource r WHERE r.rarity <= :rarity ORDER BY RAND() LIMIT :amount")
+    public List<Resource> findRandomResources(int amount, int rarity);
 }
--- city-service/src/main/java/io/github/xpakx/habitcity/shop/ShopServiceImpl.java
+++ city-service/src/main/java/io/github/xpakx/habitcity/shop/ShopServiceImpl.java
@@ -1,32 +1,50 @@
 package io.github.xpakx.habitcity.shop;
 
+import io.github.xpakx.habitcity.resource.Resource;
+import io.github.xpakx.habitcity.resource.ResourceRepository;
 import io.github.xpakx.habitcity.shop.dto.ItemResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ShopServiceImpl implements ShopService {
     private final ShopRepository shopRepository;
     private final ShopEntryRepository entryRepository;
+    private final ResourceRepository resourceRepository;
 
     @Override
-    @Scheduled(cron = "0 0 * * * *")
+    @Scheduled(cron = "0 * * * * *")
     public void refreshShops() {
         Random random = new Random();
         entryRepository.deleteAll();
         List<Shop> shops = shopRepository.findAll();
+        List<ShopEntry> entriesToAdd = new ArrayList<>();
         for(Shop shop : shops) {
             int shopSize = random.nextInt(shop.getMaxSize()-1) + 1;
             int rarity = shop.getMaxRarity();
-
-
+            List<ShopEntry> entries = resourceRepository
+                    .findRandomResources(shopSize, rarity)
+                    .stream()
+                    .map(a -> toShopEntry(a, shop))
+                    .toList();
+            entriesToAdd.addAll(entries);
         }
+        entryRepository.saveAll(entriesToAdd);
+    }
 
+    private ShopEntry toShopEntry(Resource a, Shop shop) {
+        ShopEntry entry = new ShopEntry();
+        entry.setShop(shop);
+        entry.setResource(a);
+        entry.setAmount(1);
+        return entry;
     }
 
     @Override
