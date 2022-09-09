5a17ad5a723f69d3e70772f2841129679a84d2eb
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-09 11:06:07 +0200

    fiz(city): change error code

--- city-service/src/main/java/io/github/xpakx/habitcity/shop/error/WrongOwnerException.java
+++ city-service/src/main/java/io/github/xpakx/habitcity/shop/error/WrongOwnerException.java
@@ -4,7 +4,7 @@ package io.github.xpakx.habitcity.shop.error;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_REQUEST)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class WrongOwnerException extends RuntimeException {
     public WrongOwnerException(String message) {
         super(message);
