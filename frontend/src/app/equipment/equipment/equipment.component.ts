5bfdab392fdee050187960b26bd87725557c7a77
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-12 23:07:33 +0200

    feat(frontend): get equipment from api

--- frontend/src/app/equipment/equipment/equipment.component.ts
+++ frontend/src/app/equipment/equipment/equipment.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EquipmentEntry } from '../dto/equipment-entry';
+import { EquipmentResponse } from '../dto/equipment-response';
+import { EquipmentService } from '../equipment.service';
 
 @Component({
   selector: 'app-equipment',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
+  items: EquipmentEntry[] = [];
 
-  constructor() { }
+  constructor(private eqService: EquipmentService) { }
 
   ngOnInit(): void {
+    this.eqService.getEquipment().subscribe({
+      next: (response: EquipmentResponse) => this.saveEquipment(response)
+    });
   }
+
+  saveEquipment(response: EquipmentResponse): void {
+    this.items = response.items;
+  }
+
 
 }
