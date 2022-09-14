c4d8fb1c06be59b2a5138bfc682290aa03b7a326
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-14 12:26:14 +0200

    feat(frontend): add navigation to shops and equipment

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContextsViewComponent } from './contexts-view/contexts-view.component';
+import { EquipmentComponent } from './equipment/equipment/equipment.component';
 import { ContextViewComponent } from './habit/context-view/context-view.component';
 import { DailyContextViewComponent } from './habit/daily-context-view/daily-context-view.component';
 import { DailyViewComponent } from './habit/daily-view/daily-view.component';
+import { ShopComponent } from './shop/shop/shop.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 
@@ -14,6 +16,8 @@ const routes: Routes = [
   { path: 'contexts', component: ContextsViewComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'equipment', component: EquipmentComponent },
+  { path: 'shop/:id', component: ShopComponent },
 ];
 
 @NgModule({
