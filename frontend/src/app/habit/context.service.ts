54c47161ea671c8ef6bc9ca9e8678557c56b96aa
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-14 12:18:13 +0200

    fix(frontend): add jwt tokens to requests in context service

--- frontend/src/app/habit/context.service.ts
+++ frontend/src/app/habit/context.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { JwtService } from '../common/jwt-service';
 import { ContextDetails } from './dto/context-details';
 import { ContextRequest } from './dto/context-request';
 import { Habit } from './dto/habit';
@@ -10,28 +11,30 @@ import { HabitContext } from './dto/habit-context';
 @Injectable({
   providedIn: 'root'
 })
-export class ContextService {
+export class ContextService extends JwtService {
   private apiServerUrl = environment.apiServerUrl;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    super();
+  }
 
   public addContext(request: ContextRequest): Observable<HabitContext> {
-    return this.http.post<HabitContext>(`${this.apiServerUrl}/context`, request);
+    return this.http.post<HabitContext>(`${this.apiServerUrl}/context`, request, { headers: this.getHeaders() });
   }
 
   public getDailyHabits(contextId: number):  Observable<Habit[]> {
-    return this.http.get<Habit[]>(`${this.apiServerUrl}/context/${contextId}/habit/daily`);
+    return this.http.get<Habit[]>(`${this.apiServerUrl}/context/${contextId}/habit/daily`, { headers: this.getHeaders() });
   }
 
   public getHabitsForDate(contextId: number, date: Date):  Observable<Habit[]> {
-    return this.http.get<Habit[]>(`${this.apiServerUrl}/context/${contextId}/habit/daily?date=${date}`);
+    return this.http.get<Habit[]>(`${this.apiServerUrl}/context/${contextId}/habit/daily?date=${date}`, { headers: this.getHeaders() });
   }
 
   public getHabitsForContext(contextId: number):  Observable<Habit[]> {
-    return this.http.get<Habit[]>(`${this.apiServerUrl}/context/${contextId}/habit`);
+    return this.http.get<Habit[]>(`${this.apiServerUrl}/context/${contextId}/habit`, { headers: this.getHeaders() });
   }
 
   public getContexts(): Observable<ContextDetails[]> {
-    return this.http.get<ContextDetails[]>(`${this.apiServerUrl}/context/all`);
+    return this.http.get<ContextDetails[]>(`${this.apiServerUrl}/context/all`, { headers: this.getHeaders() });
   }
 }
