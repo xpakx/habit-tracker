39ec434e0fcfcdba044b89dd4b16186a654ac0aa
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-17 21:22:21 +0200

    feat(frontend): add option to craft item

--- frontend/src/app/equipment/dto/craft-elem.ts
+++ frontend/src/app/equipment/dto/craft-elem.ts
@@ -1,3 +1,3 @@
 export interface CraftElem {
-    id: number;
+    id: number|undefined;
 }
--- frontend/src/app/equipment/equipment/equipment.component.ts
+++ frontend/src/app/equipment/equipment/equipment.component.ts
@@ -1,8 +1,12 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { DndDropEvent } from 'ngx-drag-drop'
+import { ItemResponse } from 'src/app/shop/dto/item-response';
+import { CraftRequest } from '../dto/craft-request';
 import { EquipmentEntry } from '../dto/equipment-entry';
 import { EquipmentResponse } from '../dto/equipment-response';
 import { EquipmentService } from '../equipment.service';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-equipment',
@@ -19,7 +23,7 @@ export class EquipmentComponent implements OnInit {
     this.empty, this.empty, this.empty
   ];
 
-  constructor(private eqService: EquipmentService) { }
+  constructor(private eqService: EquipmentService, private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.eqService.getEquipment().subscribe({
@@ -47,5 +51,35 @@ export class EquipmentComponent implements OnInit {
 
   reset(num: number) {
     this.craftSlots[num] = this.empty;
+  }
+
+  craft(): void {
+    this.recipeService.craft(this.createRequest()).subscribe({
+      next: (response: ItemResponse) => this.onCraft(response),
+      error: (error: HttpErrorResponse) => this.onCraftError(error),
+    });
+  }
+
+  onCraft(response: ItemResponse): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onCraftError(error: HttpErrorResponse): void {
+    throw new Error('Method not implemented.');
+  }
+
+  createRequest(): CraftRequest {
+    return {
+      amount: 1, 
+      elem1: {id: this.craftSlots[0].id},
+      elem2: {id: this.craftSlots[1].id},
+      elem3: {id: this.craftSlots[2].id},
+      elem4: {id: this.craftSlots[3].id},
+      elem5: {id: this.craftSlots[4].id},
+      elem6: {id: this.craftSlots[5].id},
+      elem7: {id: this.craftSlots[6].id},
+      elem8: {id: this.craftSlots[7].id},
+      elem9: {id: this.craftSlots[8].id},
+    }
   }
 }
