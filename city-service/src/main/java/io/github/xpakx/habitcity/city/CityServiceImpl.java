fd9059adaa76d49364887fbfe13c8445363d80da
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-19 12:35:44 +0200

    feta(city): let user build new buildings

--- city-service/src/main/java/io/github/xpakx/habitcity/city/CityServiceImpl.java
+++ city-service/src/main/java/io/github/xpakx/habitcity/city/CityServiceImpl.java
@@ -1,11 +1,15 @@
 package io.github.xpakx.habitcity.city;
 
+import io.github.xpakx.habitcity.building.Building;
 import io.github.xpakx.habitcity.building.BuildingRepository;
 import io.github.xpakx.habitcity.city.dto.BuildingRequest;
 import io.github.xpakx.habitcity.city.dto.BuildingResponse;
-import io.github.xpakx.habitcity.equipment.EquipmentService;
+import io.github.xpakx.habitcity.equipment.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -14,9 +18,30 @@ public class CityServiceImpl implements CityService {
     private final CityBuildingRepository cityBuildingRepository;
     private final BuildingRepository buildingRepository;
     private final EquipmentService equipmentService;
+    private final UserEquipmentRepository equipmentRepository;
+    private final EquipmentEntryRepository entryRepository;
 
     @Override
     public BuildingResponse build(BuildingRequest request, Long cityId, Long userId) {
+        UserEquipment eq = equipmentRepository.getByUserId(userId).orElseThrow();
+        List<EquipmentEntry> eqEntries = entryRepository.getByEquipmentId(eq.getId());
+
+        Building building = eqEntries.stream()
+                .map(EquipmentEntry::getBuilding)
+                .filter(Objects::nonNull)
+                .filter((a) -> Objects.equals(a.getId(), request.getBuildingId()))
+                .findFirst()
+                .orElseThrow();
+
+        // TODO: subtract resources
+
+        // TODO: test city size
+
+        CityBuilding cityBuilding = new CityBuilding();
+        cityBuilding.setBuilding(building);
+        cityBuilding.setCity(cityRepository.getReferenceById(cityId));
+        cityBuildingRepository.save(cityBuilding);
+
         return null;
     }
 }
