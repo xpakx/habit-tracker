58278357263612a63cf17fe823a76024fa748f0a
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-19 14:24:37 +0200

    chore(city): cache recipes in redis

--- city-service/src/main/java/io/github/xpakx/habitcity/crafting/RecipeServiceImpl.java
+++ city-service/src/main/java/io/github/xpakx/habitcity/crafting/RecipeServiceImpl.java
@@ -3,6 +3,7 @@ package io.github.xpakx.habitcity.crafting;
 import io.github.xpakx.habitcity.crafting.dto.CraftRequest;
 import io.github.xpakx.habitcity.crafting.error.NoSuchRecipeException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +12,24 @@ public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
 
     @Override
+    @Cacheable(cacheNames = "recipes", key = "T(io.github.xpakx.habitcity.crafting.RecipeServiceImpl).getCacheKey(#request)")
     public Recipe getRecipe(CraftRequest request) {
         return recipeRepository.getRecipeByResources(
                 request.getElem1().getId(), request.getElem2().getId(), request.getElem3().getId(),
                 request.getElem4().getId(), request.getElem5().getId(), request.getElem6().getId(),
                 request.getElem7().getId(), request.getElem8().getId(), request.getElem9().getId()
         ).orElseThrow(NoSuchRecipeException::new);
+    }
+
+    public static String getCacheKey(CraftRequest request){
+        return "recipe_" + request.getElem1().getId() + '_' +
+                request.getElem2().getId() + '_' +
+                request.getElem3().getId() + '_' +
+                request.getElem4().getId() + '_' +
+                request.getElem5().getId() + '_' +
+                request.getElem6().getId() + '_' +
+                request.getElem7().getId() + '_' +
+                request.getElem8().getId() + '_' +
+                request.getElem9().getId();
     }
 }
