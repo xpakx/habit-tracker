8cdde6413b7fd005cf9c5acabb5bcccb08f9f0f2
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-19 13:58:54 +0200

    chore(city): configure redis caches

--- city-service/src/main/java/io/github/xpakx/habitcity/config/CacheConfig.java
+++ city-service/src/main/java/io/github/xpakx/habitcity/config/CacheConfig.java
@@ -1,9 +1,30 @@
 package io.github.xpakx.habitcity.config;
 
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(60))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+        return (builder) -> builder
+                .withCacheConfiguration("recipes",
+                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(240)));
+    }
 }
