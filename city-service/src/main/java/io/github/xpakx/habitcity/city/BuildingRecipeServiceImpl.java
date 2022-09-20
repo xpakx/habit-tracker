41cd7f5c1b017c68edb76c9adac42c2d2b97ede9
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-20 11:53:54 +0200

    feat(city): add caching for building recipes

--- city-service/src/main/java/io/github/xpakx/habitcity/city/BuildingRecipeServiceImpl.java
+++ city-service/src/main/java/io/github/xpakx/habitcity/city/BuildingRecipeServiceImpl.java
@@ -3,6 +3,7 @@ package io.github.xpakx.habitcity.city;
 import io.github.xpakx.habitcity.building.BuildingRecipeElemRepository;
 import io.github.xpakx.habitcity.building.dto.CraftBuildingElem;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,6 +14,7 @@ public class BuildingRecipeServiceImpl implements BuildingRecipeService {
     private final BuildingRecipeElemRepository recipeRepository;
 
     @Override
+    @Cacheable(cacheNames = "b_recipes", key = "'b_recipe_'.concat(#buildingId)")
     public List<CraftBuildingElem> getRecipe(Long buildingId) {
         return recipeRepository.findByBuildingId(buildingId);
     }
