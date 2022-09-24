a5c1611f10e42e875875d4d6c9d2592a798b5d40
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-24 12:30:10 +0200

    feat(frontend): let user get overall stats

--- frontend/src/app/stats/overall-stats/overall-stats.component.ts
+++ frontend/src/app/stats/overall-stats/overall-stats.component.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { StatsResponse } from '../dto/stats-response';
+import { StatsService } from '../stats.service';
 
 @Component({
   selector: 'app-overall-stats',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overall-stats.component.css']
 })
 export class OverallStatsComponent implements OnInit {
+  stats?: StatsResponse;
 
-  constructor() { }
+  constructor(private statsService: StatsService) { }
 
   ngOnInit(): void {
+    this.statsService.getStats().subscribe({
+      next: (response: StatsResponse) => this.onSuccess(response),
+      error: (error: HttpErrorResponse) => this.onError(error)
+    })
+  }
+
+  onSuccess(response: StatsResponse) {
+    this.stats = response;
+  }
+
+  onError(error: HttpErrorResponse) {
+
   }
 
 }
