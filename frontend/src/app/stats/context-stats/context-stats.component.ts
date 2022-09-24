c97ad648bab46102a25f1c63e317e18e112f7a75
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-24 12:32:14 +0200

    feat(frontend): let user get stats for context

--- frontend/src/app/stats/context-stats/context-stats.component.ts
+++ frontend/src/app/stats/context-stats/context-stats.component.ts
@@ -1,4 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { StatsResponse } from '../dto/stats-response';
+import { StatsService } from '../stats.service';
 
 @Component({
   selector: 'app-context-stats',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./context-stats.component.css']
 })
 export class ContextStatsComponent implements OnInit {
+  stats?: StatsResponse;
 
-  constructor() { }
+  constructor(private statsService: StatsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(routeParams => {
+      let id: number | undefined = routeParams['id'];
+      if(id) {
+        this.getStats(routeParams['id']);
+      }
+    }); 
   }
 
+  private getStats(id: number) {
+    this.statsService.getStatsForContext(id).subscribe({
+      next: (response: StatsResponse) => this.onSuccess(response),
+      error: (error: HttpErrorResponse) => this.onError(error)
+    });
+  }
+
+  onSuccess(response: StatsResponse) {
+    this.stats = response;
+  }
+
+  onError(error: HttpErrorResponse) {
+
+  }
 }
