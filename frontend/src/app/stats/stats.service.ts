fa7a8e7ef65d203f3619caa206710c1233b72489
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-24 12:24:20 +0200

    feat(frontend): add service methods for gettins stats

---
+++ frontend/src/app/stats/stats.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { JwtService } from '../common/jwt-service';
+import { StatsResponse } from './dto/stats-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StatsService extends JwtService{
+  private apiServerUrl = environment.apiServerUrl;
+
+  constructor(private http: HttpClient) { 
+    super();
+  }
+
+  public getStats(shopId: number): Observable<StatsResponse> {
+    return this.http.get<StatsResponse>(`${this.apiServerUrl}/habit/stats`, { headers: this.getHeaders() });
+  }
+
+  public getStatsForContext(contextId: number): Observable<StatsResponse> {
+    return this.http.get<StatsResponse>(`${this.apiServerUrl}/context/${contextId}/stats`, { headers: this.getHeaders() });
+  }
+
+  public getStatsForHabit(habitId: number): Observable<StatsResponse> {
+    return this.http.get<StatsResponse>(`${this.apiServerUrl}/habit/${habitId}/stats`, { headers: this.getHeaders() });
+  }
+}
