90b94eb38c812c1279e6872db3f4cf4ad1c7fe36
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-25 14:24:57 +0200

    test(habit): test adding new habits

--- habit-service/src/test/java/io/github/xpakx/habittracker/habit/HabitControllerTest.java
+++ habit-service/src/test/java/io/github/xpakx/habittracker/habit/HabitControllerTest.java
@@ -1,10 +1,20 @@
 package io.github.xpakx.habittracker.habit;
 
+import io.github.xpakx.habittracker.habit.dto.HabitRequest;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.http.HttpStatus.*;
+
+import static io.restassured.RestAssured.given;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class HabitControllerTest {
@@ -22,11 +32,59 @@ class HabitControllerTest {
     @BeforeEach
     void setUp() {
         baseUrl = "http://localhost".concat(":").concat(port + "");
+        userId = 1L;
     }
 
     @AfterEach
     void tearDown() {
         habitRepository.deleteAll();
         contextRepository.deleteAll();
+    }
+
+    @Test
+    void shouldRespondWith401ToAddHabitIfNoUserIdGiven() {
+        when()
+                .post(baseUrl + "/habit")
+        .then()
+                .statusCode(UNAUTHORIZED.value());
+    }
+
+    @Test
+    void shouldNotAddNewHabitToNonexistentContext() {
+        HabitRequest request = getHabitRequest("habit1", "trigger1", 3L);
+        given()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .header(getHeaderForUserId(userId))
+        .when()
+                .post(baseUrl + "/habit")
+        .then()
+                .statusCode(NOT_FOUND.value());
+    }
+
+    private Header getHeaderForUserId(Long userId) {
+        return new Header("id", userId.toString());
+    }
+
+    @Test
+    void shouldAddNewHabit() {
+        HabitRequest request = getHabitRequest("habit1", "trigger1", null);
+        given()
+                .contentType(ContentType.JSON)
+                .body(request)
+                .header(getHeaderForUserId(userId))
+        .when()
+                .post(baseUrl + "/habit")
+        .then()
+                .statusCode(CREATED.value())
+                .body("name", equalTo(request.getName()));
+    }
+
+    private HabitRequest getHabitRequest(String name, String triggerName, Long contextId) {
+        HabitRequest request = new HabitRequest();
+        request.setName(name);
+        request.setContextId(contextId);
+        request.setTriggerName(triggerName);
+        return request;
     }
 }
