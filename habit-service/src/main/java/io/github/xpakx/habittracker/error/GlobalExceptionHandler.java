5d1a0c08cd9abd18f4a6abe9c05ca98fb2370f54
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-25 14:58:07 +0200

    feat(habit): add exception handler

---
+++ habit-service/src/main/java/io/github/xpakx/habittracker/error/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package io.github.xpakx.habittracker.error;
+
+import io.github.xpakx.habittracker.error.dto.ErrorResponse;
+import io.github.xpakx.habittracker.habit.error.NoSuchObjectException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { NoSuchObjectException.class })
+    protected ResponseEntity<Object> handleNoSuchObjectException(RuntimeException ex, WebRequest request) {
+        ErrorResponse errorBody = new ErrorResponse();
+        errorBody.setMessage(ex.getMessage());
+        return handleExceptionInternal(
+                ex,
+                errorBody,
+                new HttpHeaders(),
+                HttpStatus.NOT_FOUND,
+                request
+        );
+    }
+}
---
+++ habit-service/src/main/java/io/github/xpakx/habittracker/error/dto/ErrorResponse.java
@@ -0,0 +1,10 @@
+package io.github.xpakx.habittracker.error.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorResponse {
+    private String message;
+}
