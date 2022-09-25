5ca51483acafeb1b16f7d7b87031cc53f68773d4
xpakx <55586470+xpakx@users.noreply.github.com>
2022-09-25 15:41:17 +0200

    refactor(habit): delete unnecessary field

--- habit-service/src/main/java/io/github/xpakx/habittracker/error/GlobalExceptionHandler.java
+++ habit-service/src/main/java/io/github/xpakx/habittracker/error/GlobalExceptionHandler.java
@@ -10,12 +10,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.annotation.ResponseStatusExceptionResolver;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    ResponseStatusExceptionResolver resolver;
 
     @ExceptionHandler(value = NoSuchObjectException.class)
     protected ResponseEntity<Object> handleNoSuchObjectException(RuntimeException ex, WebRequest request) {
