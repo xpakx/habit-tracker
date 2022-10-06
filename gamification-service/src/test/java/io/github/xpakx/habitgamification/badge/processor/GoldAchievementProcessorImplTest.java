8cb7df04bad51cfd2ece2c2f17144a17b9f06610
xpakx <55586470+xpakx@users.noreply.github.com>
2022-10-06 21:25:25 +0200

    test(gamification): test returning silver and gold badges with random events

--- gamification-service/src/test/java/io/github/xpakx/habitgamification/badge/processor/GoldAchievementProcessorImplTest.java
+++ gamification-service/src/test/java/io/github/xpakx/habitgamification/badge/processor/GoldAchievementProcessorImplTest.java
@@ -5,14 +5,19 @@ import io.github.xpakx.habitgamification.gamification.dto.HabitCompletionEvent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.Optional;
+import java.util.Random;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GoldAchievementProcessorImplTest {
     private AchievementProcessor processor;
+    private final static Random rnd = new Random(210832879274L);
 
     @BeforeEach
     void setUp() {
@@ -40,6 +45,41 @@ class GoldAchievementProcessorImplTest {
         HabitCompletionEvent event = new HabitCompletionEvent();
         Optional<Badge> result = processor.process(event, experience);
         assertTrue(result.isEmpty());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideRandomEvents")
+    void eventContentShouldNotInfluenceResultIfExpHigherThan5000(HabitCompletionEvent event) {
+        Optional<Badge> result = processor.process(event, 15000);
+        assertTrue(result.isPresent());
+        assertEquals(Badge.GOLD, result.get());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideRandomEvents")
+    void eventContentShouldNotInfluenceResultIfExpLowerThan5000(HabitCompletionEvent event) {
+        Optional<Badge> result = processor.process(event, 10);
+        assertTrue(result.isEmpty());
+    }
+
+    private static Stream<Arguments> provideRandomEvents() {
+        return Stream.of(
+                Arguments.of(constructRandomEvent()),
+                Arguments.of(constructRandomEvent()),
+                Arguments.of(constructRandomEvent()),
+                Arguments.of(constructRandomEvent()),
+                Arguments.of(constructRandomEvent()),
+                Arguments.of(constructRandomEvent())
+        );
+    }
+
+    private static HabitCompletionEvent constructRandomEvent() {
+        HabitCompletionEvent event = new HabitCompletionEvent();
+        event.setCompletionId(rnd.nextLong());
+        event.setUserId(rnd.nextLong());
+        event.setHabitId(rnd.nextLong());
+        event.setDifficulty(rnd.nextInt());
+        return event;
     }
 
 }
