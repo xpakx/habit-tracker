f29b5d8c6ff7ecb8ce42659e1ddfc146a45b43c5
xpakx <55586470+xpakx@users.noreply.github.com>
2022-10-06 20:56:18 +0200

    chore(gamification): add dependencies for rest assured

--- habit-service/src/test/java/io/github/xpakx/habittracker/habit/HabitCompletionControllerTest.java
+++ habit-service/src/test/java/io/github/xpakx/habittracker/habit/HabitCompletionControllerTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
