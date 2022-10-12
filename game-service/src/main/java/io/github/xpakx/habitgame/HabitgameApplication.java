81875abd4351511bb9f34caa068ea371b74debf9
xpakx <55586470+xpakx@users.noreply.github.com>
2022-10-12 22:11:26 +0200

    chore(game): init

---
+++ game-service/src/main/java/io/github/xpakx/habitgame/HabitgameApplication.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.habitgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitgameApplication.class, args);
+	}
+
+}
---
+++ game-service/src/test/java/io/github/xpakx/habitgame/HabitgameApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xpakx.habitgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
