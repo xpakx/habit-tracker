4de7b91402cb6eaa27d96add937431c11fb3f0b0
xpakx <55586470+xpakx@users.noreply.github.com>
2022-10-20 11:13:50 +0200

    test(city): test getting all cities for user

---
+++ city-service/src/test/java/io/github/xpakx/habitcity/city/CityControllerTest.java
@@ -0,0 +1,108 @@
+package io.github.xpakx.habitcity.city;
+
+import io.github.xpakx.habitcity.config.SchedulerConfig;
+import io.restassured.http.Header;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CityControllerTest {
+    @LocalServerPort
+    private int port;
+    private String baseUrl;
+    private Long userId;
+
+    private final static int FULL_STACK = 64;
+
+    @Autowired
+    CityRepository cityRepository;
+    @MockBean
+    SchedulerConfig config;
+
+    @BeforeEach
+    void setUp() {
+        baseUrl = "http://localhost".concat(":").concat(port + "");
+        userId = 1L;
+    }
+
+    @AfterEach
+    void tearDown() {
+        cityRepository.deleteAll();
+    }
+
+    private Header getHeaderForUserId(Long userId) {
+        return new Header("id", userId.toString());
+    }
+
+    @Test
+    void shouldRespondWith401ToGetCitiesIfNoUserIdGiven() {
+        when()
+                .get(baseUrl + "/city/all")
+        .then()
+                .statusCode(UNAUTHORIZED.value());
+    }
+
+    @Test
+    void shouldRespondWithEmptyCityList() {
+        given()
+                .header(getHeaderForUserId(userId))
+        .when()
+                .get(baseUrl + "/city/all")
+        .then()
+                .statusCode(OK.value())
+                .body("$", hasSize(0));
+    }
+
+    @Test
+    void shouldRespondWithCityListWithOneElem() {
+        Long cityId = createCity();
+        given()
+                .header(getHeaderForUserId(userId))
+        .when()
+                .get(baseUrl + "/city/all")
+        .then()
+                .statusCode(OK.value())
+                .body("$", hasSize(1))
+                .body("id", hasItem(cityId.intValue()));
+    }
+
+    @Test
+    void shouldRespondWithCityList() {
+        Long city1Id = createCity();
+        Long city2Id = createCity();
+        Long city3Id = createCity();
+        Long city4Id = createCity(userId+1);
+        given()
+                .header(getHeaderForUserId(userId))
+        .when()
+                .get(baseUrl + "/city/all")
+        .then()
+                .statusCode(OK.value())
+                .body("$", hasSize(3))
+                .body("id", hasItems(city1Id.intValue(), city2Id.intValue(), city3Id.intValue()))
+                .body("id", not(hasItem(city4Id.intValue())));
+    }
+
+    private Long createCity() {
+        return createCity(userId);
+    }
+
+    private Long createCity(Long userId) {
+        City city = new City();
+        city.setMaxSize(10);
+        city.setUserId(userId);
+        return cityRepository.save(city).getId();
+    }
+}
