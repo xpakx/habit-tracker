be1ac1d9a49e9a7e69efdb4e665ea0e7f8ac38b3
xpakx <55586470+xpakx@users.noreply.github.com>
2022-10-20 11:08:29 +0200

    test(city): test getting equipment

--- city-service/src/test/java/io/github/xpakx/habitcity/equipment/EquipmentControllerTest.java
+++ city-service/src/test/java/io/github/xpakx/habitcity/equipment/EquipmentControllerTest.java
@@ -1,7 +1,9 @@
 package io.github.xpakx.habitcity.equipment;
 
 import io.github.xpakx.habitcity.config.SchedulerConfig;
+import io.github.xpakx.habitcity.money.Money;
 import io.github.xpakx.habitcity.money.MoneyRepository;
+import io.github.xpakx.habitcity.resource.Resource;
 import io.github.xpakx.habitcity.resource.ResourceRepository;
 import io.github.xpakx.habitcity.shop.ShopEntryRepository;
 import io.github.xpakx.habitcity.shop.ShopRepository;
@@ -16,9 +18,11 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.HttpStatus.OK;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EquipmentControllerTest {
@@ -74,5 +78,61 @@ class EquipmentControllerTest {
                 .get(baseUrl + "/equipment")
         .then()
                 .statusCode(NOT_FOUND.value());
+    }
+
+    private void createEquipment() {
+        UserEquipment equipment = new UserEquipment();
+        equipment.setMaxSize(50);
+        equipment.setUserId(userId);
+        equipmentRepository.save(equipment);
+    }
+
+    private void addItemToEquipment(Long entryId, int amount) {
+        EquipmentEntry entry = new EquipmentEntry();
+        entry.setAmount(amount);
+        entry.setEquipment(equipmentRepository.getByUserId(userId).orElse(null));
+        entry.setResource(resourceRepository.getReferenceById(entryId));
+        entryRepository.save(entry);
+    }
+
+    private Long addResource(String itemName) {
+        Resource res = new Resource();
+        res.setName(itemName);
+        res.setBaseCost(5);
+        res.setCode(itemName.toUpperCase());
+        res.setMaxStock(FULL_STACK);
+        res.setRarity(0);
+        return resourceRepository.save(res).getId();
+    }
+
+    @Test
+    void shouldRespondWithEmptyEquipment() {
+        createEquipment();
+        given()
+                .header(getHeaderForUserId(userId))
+        .when()
+                .get(baseUrl + "/equipment")
+        .then()
+                .statusCode(OK.value())
+                .body("items", hasSize(0));
+    }
+
+    @Test
+    void shouldReturnListOfItemsInEquipment() {
+        createEquipment();
+        Long item1 = addResource("item1");
+        Long item2 = addResource("item2");
+        Long item3 = addResource("item3");
+        addItemToEquipment(item1, 40);
+        addItemToEquipment(item1, 30);
+        addItemToEquipment(item2, 64);
+        addItemToEquipment(item3, 15);
+        given()
+                .header(getHeaderForUserId(userId))
+        .when()
+                .get(baseUrl + "/equipment")
+        .then()
+                .statusCode(OK.value())
+                .body("items", hasSize(4));
     }
 }
