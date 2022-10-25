7c979ea8b0f9b9f40925a49c633d4a3564240fab
xpakx <55586470+xpakx@users.noreply.github.com>
2022-10-25 14:29:18 +0200

    test(city): test crafteing while no equipment or no required building found

--- city-service/src/test/java/io/github/xpakx/habitcity/crafting/CraftControllerTest.java
+++ city-service/src/test/java/io/github/xpakx/habitcity/crafting/CraftControllerTest.java
@@ -1,5 +1,7 @@
 package io.github.xpakx.habitcity.crafting;
 
+import io.github.xpakx.habitcity.building.Building;
+import io.github.xpakx.habitcity.building.BuildingRepository;
 import io.github.xpakx.habitcity.city.CityBuildingRepository;
 import io.github.xpakx.habitcity.city.CityService;
 import io.github.xpakx.habitcity.config.SchedulerConfig;
@@ -7,6 +9,7 @@ import io.github.xpakx.habitcity.crafting.dto.CraftRequest;
 import io.github.xpakx.habitcity.crafting.dto.CraftRequestElem;
 import io.github.xpakx.habitcity.equipment.EquipmentEntryRepository;
 import io.github.xpakx.habitcity.equipment.EquipmentService;
+import io.github.xpakx.habitcity.equipment.UserEquipment;
 import io.github.xpakx.habitcity.equipment.UserEquipmentRepository;
 import io.github.xpakx.habitcity.money.MoneyRepository;
 import io.github.xpakx.habitcity.resource.Resource;
@@ -26,8 +29,7 @@ import java.util.List;
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.HttpStatus.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CraftControllerTest {
@@ -49,6 +51,8 @@ class CraftControllerTest {
     ResourceRepository resourceRepository;
     @Autowired
     CityBuildingRepository cityBuildingRepository;
+    @Autowired
+    BuildingRepository buildingRepository;
     @MockBean
     SchedulerConfig config;
 
@@ -63,6 +67,7 @@ class CraftControllerTest {
         entryRepository.deleteAll();
         resourceRepository.deleteAll();
         cityBuildingRepository.deleteAll();
+        buildingRepository.deleteAll();
         equipmentRepository.deleteAll();
         recipeRepository.deleteAll();
     }
@@ -120,6 +125,69 @@ class CraftControllerTest {
         CraftRequestElem elem = new CraftRequestElem();
         elem.setId(id);
         return elem;
+    }
+
+    @Test
+    void shouldRespondWith404ToCraftIfNotEquipmentFound() {
+        List<Long> recipe = List.of(addItem("item1"));
+        CraftRequest request = createCraftRequest(recipe);
+        addRecipe(recipe, addItem("product"), addBuilding("building1"));
+        given()
+                .header(getHeaderForUserId(userId))
+                .contentType(ContentType.JSON)
+                .body(request)
+        .when()
+                .post(baseUrl + "/craft")
+        .then()
+                .statusCode(NOT_FOUND.value());
+    }
+
+    @Test
+    void shouldRespondWith400ToCraftIfRequiredBuildingIsNotBuilt() {
+        List<Long> recipe = List.of(addItem("item1"));
+        createEquipment();
+        CraftRequest request = createCraftRequest(recipe);
+        addRecipe(recipe, addItem("product"), addBuilding("building1"));
+        given()
+                .header(getHeaderForUserId(userId))
+                .contentType(ContentType.JSON)
+                .body(request)
+        .when()
+                .post(baseUrl + "/craft")
+        .then()
+                .statusCode(BAD_REQUEST.value());
+    }
+
+
+    private void createEquipment() {
+        UserEquipment equipment = new UserEquipment();
+        equipment.setMaxSize(100);
+        equipment.setUserId(userId);
+        equipmentRepository.save(equipment);
+    }
+
+    private void addRecipe(List<Long> items, Long item, Long building) {
+        Recipe recipe = new Recipe();
+        recipe.setRequiredBuilding(buildingRepository.getReferenceById(building));
+        recipe.setResource1(items.size()>0 ? resourceRepository.getReferenceById(items.get(0)) : null);
+        recipe.setResource2(items.size()>1 ? resourceRepository.getReferenceById(items.get(1)) : null);
+        recipe.setResource3(items.size()>2 ? resourceRepository.getReferenceById(items.get(2)) : null);
+        recipe.setResource4(items.size()>3 ? resourceRepository.getReferenceById(items.get(3)) : null);
+        recipe.setResource5(items.size()>4 ? resourceRepository.getReferenceById(items.get(4)) : null);
+        recipe.setResource6(items.size()>5 ? resourceRepository.getReferenceById(items.get(5)) : null);
+        recipe.setResource7(items.size()>6 ? resourceRepository.getReferenceById(items.get(6)) : null);
+        recipe.setResource8(items.size()>7 ? resourceRepository.getReferenceById(items.get(7)) : null);
+        recipe.setResource9(items.size()>8 ? resourceRepository.getReferenceById(items.get(8)) : null);
+        recipe.setResource(resourceRepository.getReferenceById(item));
+        recipeRepository.save(recipe);
+    }
+
+    private Long addBuilding(String name) {
+        Building building = new Building();
+        building.setName(name);
+        building.setBaseCost(0);
+        building.setRarity(0);
+        return buildingRepository.save(building).getId();
     }
 
 
