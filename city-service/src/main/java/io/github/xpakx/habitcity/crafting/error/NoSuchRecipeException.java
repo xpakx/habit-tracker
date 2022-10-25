75ac30692685e0513a4e27a123d684f2de1f31f5
xpakx <55586470+xpakx@users.noreply.github.com>
2022-10-25 14:20:50 +0200

    fix(city): fix error code for wrong recipe

--- city-service/src/main/java/io/github/xpakx/habitcity/crafting/error/NoSuchRecipeException.java
+++ city-service/src/main/java/io/github/xpakx/habitcity/crafting/error/NoSuchRecipeException.java
@@ -4,7 +4,7 @@ package io.github.xpakx.habitcity.crafting.error;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NoSuchRecipeException extends RuntimeException {
     public NoSuchRecipeException(String message) {
         super(message);
