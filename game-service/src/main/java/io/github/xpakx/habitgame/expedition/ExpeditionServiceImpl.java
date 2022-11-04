b1e767320feec3c628ef18c90e18f3a9f6d1f93a
xpakx <55586470+xpakx@users.noreply.github.com>
2022-11-04 16:57:09 +0100

    feat(game): add ships and cargo to db

--- game-service/src/main/java/io/github/xpakx/habitgame/expedition/ExpeditionServiceImpl.java
+++ game-service/src/main/java/io/github/xpakx/habitgame/expedition/ExpeditionServiceImpl.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,12 +24,47 @@ public class ExpeditionServiceImpl implements ExpeditionService {
 
     @Override
     public void addExpedition(ExpeditionEvent event) {
-        Island island = islandRepository.findById(event.getIslandId()).orElse(null);
+        Island island = islandRepository.findById(event.getIslandId()).orElse(getDefaultIsland());
         Expedition expedition = new Expedition();
         expedition.setUserId(event.getUserId());
         expedition.setStart(LocalDateTime.now());
         expedition.setStart(LocalDateTime.now().plusHours(10));
         expedition.setDestination(island);
         expeditionRepository.save(expedition);
+        List<Ship> ships = event.getShips().stream().map(a -> toShip(a, expedition)).toList();
+        Map<Long, Ship> shipMap = shipRepository.saveAll(ships).stream()
+                .collect(Collectors.toMap(Ship::getShipId, a -> a));
+        List<Item> items = new ArrayList<>();
+        for(EventShip ship : event.getShips()) {
+            items.addAll(ship.getCargo().stream().map(a -> toItem(a, shipMap.get(ship.getShipId()))).toList());
+        }
+        itemRepository.saveAll(items);
+
+    }
+
+    private Item toItem(Cargo cargo, Ship ship) {
+        Item item = new Item();
+        item.setCode(cargo.getCode());
+        item.setName(cargo.getName());
+        item.setAmount(cargo.getAmount());
+        item.setRarity(cargo.getRarity());
+        item.setResourceId(cargo.getResourceId());
+        item.setShip(ship);
+        return item;
+    }
+
+    private Ship toShip(EventShip eventShip, Expedition expedition) {
+        Ship ship = new Ship();
+        ship.setShipId(eventShip.getShipId());
+        ship.setCode(eventShip.getCode());
+        ship.setMaxCargo(eventShip.getMaxCargo());
+        ship.setName(eventShip.getName());
+        ship.setSize(eventShip.getSize());
+        ship.setExpedition(expedition);
+        return ship;
+    }
+
+    private Island getDefaultIsland() {
+        return null;
     }
 }
