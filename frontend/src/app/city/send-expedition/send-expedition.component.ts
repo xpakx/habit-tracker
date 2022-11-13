fb3402e2fde63ca7cb563d4f176e72cef0dac518
xpakx <55586470+xpakx@users.noreply.github.com>
2022-11-13 19:53:33 +0100

    feat(frontend): select correct city in expeditions view

--- frontend/src/app/city/send-expedition/send-expedition.component.ts
+++ frontend/src/app/city/send-expedition/send-expedition.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CityService } from '../city.service';
 
 @Component({
   selector: 'app-send-expedition',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./send-expedition.component.css']
 })
 export class SendExpeditionComponent implements OnInit {
+  cityId?: number;
 
-  constructor() { }
+  constructor(private cityService: CityService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(routeParams => {
+      this.cityId = routeParams['id'];
+      if(this.cityId) {
+        this.getExpeditions(this.cityId);
+      } 
+    }); 
+  }
+  
+  getExpeditions(cityId: number) {
+    throw new Error('Method not implemented.');
   }
 
 }
