a9fd5e2cb6af9543201b9a458338c7e2abc0fbbe
xpakx <55586470+xpakx@users.noreply.github.com>
2022-11-20 18:04:09 +0100

    test(game): test getting active expeditions

--- game-service/src/test/java/io/github/xpakx/habitgame/expedition/ExpeditionControllerTest.java
+++ game-service/src/test/java/io/github/xpakx/habitgame/expedition/ExpeditionControllerTest.java
@@ -3,9 +3,17 @@ package io.github.xpakx.habitgame.expedition;
 import io.restassured.http.Header;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ExpeditionControllerTest {
@@ -32,4 +40,43 @@ class ExpeditionControllerTest {
         return new Header("id", userId.toString());
     }
 
+    @Test
+    void shouldRespondWith401ToGetExpeditionsIfNoUserIdGiven() {
+        when()
+                .get(baseUrl + "/expedition/active")
+        .then()
+                .statusCode(UNAUTHORIZED.value());
+    }
+
+    @Test
+    void shouldRespondWithEmptyExpeditionList() {
+        given()
+                .header(getHeaderForUserId(userId))
+        .when()
+                .get(baseUrl + "/expedition/active")
+        .then()
+                .statusCode(OK.value())
+                .body("$", hasSize(0));
+    }
+
+    @Test
+    void shouldReturnListOfExpeditionsForUser() {
+        addExpedition(userId);
+        addExpedition(userId);
+        addExpedition(userId+1);
+        given()
+                .header(getHeaderForUserId(userId))
+        .when()
+                .get(baseUrl + "/expedition/active")
+        .then()
+                .statusCode(OK.value())
+                .body("$", hasSize(2));
+    }
+
+    private void addExpedition(long userId) {
+        Expedition expedition = new Expedition();
+        expedition.setUserId(userId);
+        expedition.setFinished(false);
+        expeditionRepository.save(expedition);
+    }
 }
