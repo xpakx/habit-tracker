2342587128eb91418a43149f2be593a82106c7d1
xpakx <55586470+xpakx@users.noreply.github.com>
2022-11-29 13:21:05 +0100

    test(game): test naming islands

--- game-service/src/test/java/io/github/xpakx/habitgame/island/IslandControllerTest.java
+++ game-service/src/test/java/io/github/xpakx/habitgame/island/IslandControllerTest.java
@@ -1,5 +1,7 @@
 package io.github.xpakx.habitgame.island;
 
+import io.github.xpakx.habitgame.island.dto.NamingIslandRequest;
+import io.restassured.http.ContentType;
 import io.restassured.http.Header;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,8 +11,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+import java.util.Optional;
+
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.hasItem;
 import static org.junit.jupiter.api.Assertions.*;
@@ -79,10 +84,64 @@ class IslandControllerTest {
                 .body("name", not(hasItem("island3")));
     }
 
-    private void addIsland(long userId, String islandName) {
+    private Long addIsland(long userId, String islandName) {
         Island island = new Island();
         island.setName(islandName);
         island.setUserId(userId);
-        islandRepository.save(island);
+        return islandRepository.save(island).getId();
+    }
+
+    @Test
+    void shouldRespondWith401ToNameIslandIfNoUserIdGiven() {
+        when()
+                .post(baseUrl + "/island/{islandId}/name", 1L)
+        .then()
+                .statusCode(UNAUTHORIZED.value());
+    }
+
+    @Test
+    void shouldRespondWith404ToNameIslandIfIslandDoesNotExist() {
+        given()
+                .header(getHeaderForUserId(userId))
+                .contentType(ContentType.JSON)
+                .body(getNameRequest("island"))
+        .when()
+                .post(baseUrl + "/island/{islandId}/name", 1L)
+        .then()
+                .statusCode(NOT_FOUND.value());
+    }
+
+    private NamingIslandRequest getNameRequest(String name) {
+        NamingIslandRequest request = new NamingIslandRequest();
+        request.setName(name);
+        return request;
+    }
+
+    @Test
+    void shouldNameIsland() {
+        Long islandId = addIsland(userId, "None");
+        given()
+                .header(getHeaderForUserId(userId))
+                .contentType(ContentType.JSON)
+                .body(getNameRequest("island"))
+        .when()
+                .post(baseUrl + "/island/{islandId}/name", islandId)
+        .then()
+                .statusCode(OK.value())
+                .body("islandName", equalTo("island"));
+    }
+
+    @Test
+    void shouldChangeIslandName() {
+        Long islandId = addIsland(userId, "None");
+        given()
+                .header(getHeaderForUserId(userId))
+                .contentType(ContentType.JSON)
+                .body(getNameRequest("island"))
+        .when()
+                .post(baseUrl + "/island/{islandId}/name", islandId);
+        Optional<Island> island = islandRepository.findById(islandId);
+        assertTrue(island.isPresent());
+        assertThat(island.get(), hasProperty("name", equalTo("island")));
     }
 }
