1774856912d870c462f37cadaa4e4b0bf0bd82cf
xpakx <55586470+xpakx@users.noreply.github.com>
2022-12-03 20:08:58 +0100

    feat(game): add endpoints for battles

---
+++ game-service/src/main/java/io/github/xpakx/habitgame/battle/BattleController.java
@@ -0,0 +1,40 @@
+package io.github.xpakx.habitgame.battle;
+
+import io.github.xpakx.habitgame.battle.dto.BattleResponse;
+import io.github.xpakx.habitgame.battle.dto.MoveRequest;
+import io.github.xpakx.habitgame.battle.dto.MoveResponse;
+import io.github.xpakx.habitgame.expedition.dto.ActionResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class BattleController {
+    private final BattleService service;
+
+    @GetMapping("/expedition/{expeditionId}/battle")
+    public ResponseEntity<BattleResponse> startBattle(@RequestHeader String id, @PathVariable Long expeditionId) {
+        return new ResponseEntity<>(
+                service.start(expeditionId, Long.valueOf(id)),
+                HttpStatus.OK
+        );
+    }
+
+    @PostMapping("/battle/{battleId}/position")
+    public ResponseEntity<MoveResponse> prepare(@RequestBody MoveRequest request, @RequestHeader String id, @PathVariable Long expeditionId) {
+        return new ResponseEntity<>(
+                service.prepare(request, expeditionId, Long.valueOf(id)),
+                HttpStatus.OK
+        );
+    }
+
+    @PostMapping("/battle/{battleId}/move")
+    public ResponseEntity<MoveResponse> move(@RequestBody MoveRequest request, @RequestHeader String id, @PathVariable Long expeditionId) {
+        return new ResponseEntity<>(
+                service.move(request, expeditionId, Long.valueOf(id)),
+                HttpStatus.OK
+        );
+    }
+}
--- game-service/src/main/java/io/github/xpakx/habitgame/battle/BattleService.java
+++ game-service/src/main/java/io/github/xpakx/habitgame/battle/BattleService.java
@@ -1,10 +1,11 @@
 package io.github.xpakx.habitgame.battle;
 
 import io.github.xpakx.habitgame.battle.dto.BattleResponse;
+import io.github.xpakx.habitgame.battle.dto.MoveRequest;
 import io.github.xpakx.habitgame.battle.dto.MoveResponse;
 
 public interface BattleService {
     BattleResponse start(Long expeditionId, Long userId);
-    MoveResponse move(MoveResponse request, Long battleId, Long userId);
-    MoveResponse prepare(MoveResponse request, Long battleId, Long userId);
+    MoveResponse move(MoveRequest request, Long battleId, Long userId);
+    MoveResponse prepare(MoveRequest request, Long battleId, Long userId);
 }
