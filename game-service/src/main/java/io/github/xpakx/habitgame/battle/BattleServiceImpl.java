6e55a7d05d9ba5a520229f629e0879c57fa65a01
xpakx <55586470+xpakx@users.noreply.github.com>
2022-12-03 20:09:51 +0100

    chore(game): implement interface

---
+++ game-service/src/main/java/io/github/xpakx/habitgame/battle/BattleServiceImpl.java
@@ -0,0 +1,30 @@
+package io.github.xpakx.habitgame.battle;
+
+import io.github.xpakx.habitgame.battle.dto.BattleResponse;
+import io.github.xpakx.habitgame.battle.dto.MoveRequest;
+import io.github.xpakx.habitgame.battle.dto.MoveResponse;
+import io.github.xpakx.habitgame.expedition.ExpeditionRepository;
+import io.github.xpakx.habitgame.expedition.ExpeditionResultRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BattleServiceImpl implements BattleService {
+    private final ExpeditionResultRepository resultRepository;
+    
+    @Override
+    public BattleResponse start(Long expeditionId, Long userId) {
+        return null;
+    }
+
+    @Override
+    public MoveResponse move(MoveRequest request, Long battleId, Long userId) {
+        return null;
+    }
+
+    @Override
+    public MoveResponse prepare(MoveRequest request, Long battleId, Long userId) {
+        return null;
+    }
+}
