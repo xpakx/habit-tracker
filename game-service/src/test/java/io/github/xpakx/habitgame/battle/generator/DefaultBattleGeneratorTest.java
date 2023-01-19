23c4cde8960c66568760f4bfd92ff9c4f3d955a2
xpakx <55586470+xpakx@users.noreply.github.com>
2023-01-19 22:00:11 +0100

    game(test): test generating ships for defeat mode

--- game-service/src/test/java/io/github/xpakx/habitgame/battle/generator/DefaultBattleGeneratorTest.java
+++ game-service/src/test/java/io/github/xpakx/habitgame/battle/generator/DefaultBattleGeneratorTest.java
@@ -5,14 +5,20 @@ import io.github.xpakx.habitgame.battle.BattleObjective;
 import io.github.xpakx.habitgame.battle.BattleRepository;
 import io.github.xpakx.habitgame.expedition.Expedition;
 import io.github.xpakx.habitgame.expedition.ExpeditionResult;
+import io.github.xpakx.habitgame.expedition.Ship;
 import io.github.xpakx.habitgame.expedition.ShipRepository;
+import io.github.xpakx.habitgame.ship.ShipType;
 import io.github.xpakx.habitgame.ship.ShipTypeRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -29,13 +35,17 @@ class DefaultBattleGeneratorTest {
     @Mock
     private ShipTypeRepository shipTypeRepository;
 
-    @BeforeEach
-    void setUp() {
+    private Battle getBattle() {
+        return new Battle();
+    }
+
+    private void initMocks() {
         generator = new DefaultBattleGenerator(battleRepository, shipRepository, shipTypeRepository);
     }
 
     @Test
     void shouldUseCorrectObjectiveForGeneratedBattle() {
+        initMocks();
         Battle result = generator.createBattle(getExpeditionResult());
         assertThat(result, is(notNullValue()));
         assertThat(result, hasProperty("objective", equalTo(BattleObjective.DEFEAT)));
@@ -50,6 +60,7 @@ class DefaultBattleGeneratorTest {
 
     @Test
     void generatedBattleShouldHaveCorrectInitialValues() {
+        initMocks();
         Battle result = generator.createBattle(getExpeditionResult());
         assertThat(result, is(notNullValue()));
         assertThat(result, hasProperty("finished", equalTo(false)));
@@ -59,11 +70,76 @@ class DefaultBattleGeneratorTest {
 
     @Test
     void generatedBattleShouldHaveNonZeroHeightAndWidth() {
+        initMocks();
         Battle result = generator.createBattle(getExpeditionResult());
         assertThat(result, is(notNullValue()));
         assertThat(result, hasProperty("height", is(notNullValue())));
         assertThat(result, hasProperty("height", is(greaterThan(0))));
         assertThat(result, hasProperty("width", is(notNullValue())));
         assertThat(result, hasProperty("width", is(greaterThan(0))));
+    }
+
+    @Test
+    void shouldGenerateEmptyShipListForEmptyExpedition() {
+        Mockito.when(shipRepository.findByExpeditionId(Mockito.anyLong())).thenReturn(new ArrayList<>());
+        //initShipPrototypes(1,2,3);
+        initMocks();
+        List<Ship> result = generator.generateShips(1L, getExpedition(), rnd);
+        assertThat(result, hasSize(0));
+    }
+
+    private Expedition getExpedition() {
+        Expedition expedition = new Expedition();
+        expedition.setId(1L);
+        expedition.setUserId(1L);
+        return expedition;
+    }
+
+    private void initShipPrototypes(Integer... rarities) {
+        List<ShipType> types = new ArrayList<>();
+        for(int rarity : rarities) {
+            types.add(createShipPrototype(rarity, "ship"+rarity, (long) rarity));
+        }
+        Mockito.when(shipTypeRepository.findRandomTypes(Mockito.anyInt(), Mockito.anyInt())).thenReturn(types);
+    }
+
+    private ShipType createShipPrototype(int rarity, String name, Long id) {
+        ShipType type = new ShipType();
+        type.setRarity(rarity);
+        type.setBaseSize(1);
+        type.setCode(name.toUpperCase());
+        type.setCriticalRate(5);
+        type.setHitRate(90);
+        type.setHp(20);
+        type.setStrength(3);
+        type.setId(id);
+        type.setName(name);
+        type.setImgUrl("");
+        return type;
+    }
+
+    @Test
+    void shouldGenerateShipList() {
+        Mockito.when(shipRepository.findByExpeditionId(Mockito.anyLong())).thenReturn(generateShips(1));
+        initShipPrototypes(1);
+        initMocks();
+        List<Ship> result = generator.generateShips(1L, getExpedition(), rnd);
+        assertThat(result, hasSize(greaterThan(0)));
+    }
+
+    private List<Ship> generateShips(int... rarities) {
+        List<Ship> ships = new ArrayList<>();
+        int id = 0;
+        for(int rarity : rarities) {
+            ships.add(createShip(rarity, "ship"+id++));
+        }
+        return ships;
+    }
+
+    private Ship createShip(int rarity, String name) {
+        Ship ship = new Ship();
+        ship.setName(name);
+        ship.setRarity(rarity);
+        return ship;
     }
 }
